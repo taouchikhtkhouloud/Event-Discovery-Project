56c9d1d5294fd47099626d06cdab48653a5bd43b
Oussama Louati <92301144+OssamaLouati@users.noreply.github.com>
2022-04-29 23:33:40 +0000

    up

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form action='../../post' method='post' className="form">
+        <button type="submit">
+            Connected?
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
