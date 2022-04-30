f08a56134ed21e15b93f1c2db1940d2ad81db81e
Oussama Louati <92301144+OssamaLouati@users.noreply.github.com>
2022-04-30 00:53:31 +0000

    uppp

--- clien/src/App.js
+++ clien/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <form action='../../post' method='post' className="form">
+      <form action='../../post' method='POST' className="form">
         <button type="submit">
             Connected?
         </button>
