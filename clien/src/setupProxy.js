b1a481ffa373d971c676c873b3e3157a104e7288
Oussama Louati <92301144+OssamaLouati@users.noreply.github.com>
2022-04-30 00:12:38 +0000

    dfdf

--- clien/src/setupProxy.js
+++ clien/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: "http://localhost:8090/server",
+      target: "http://localhost:8090",
       changeOrigin: true,
     })
   );
