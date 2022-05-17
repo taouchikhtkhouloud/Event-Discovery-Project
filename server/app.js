d584ecd3db48c584ca89a9bb3acfdaa293723442
taouchikhtkhouloud <92301300+taouchikhtkhouloud@users.noreply.github.com>
2022-05-17 17:22:23 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "khouloud",
+  database: "login"
+})
 
 app.get("/", (req,res) => {
     res.send("hello world");
